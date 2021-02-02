c40470b0ab601ae44cd98ea7cfac63159c4470e5
viksen <viksen_1234@hotmail.com>
2021-02-02 13:40:13 +0100

    API_Ej1

---
+++ API_Ej1/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Ej1.Models
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public int DNI { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UD24.API
 Ejercicios de API's
+
+GET: https://localhost:44342/api/Clientes
+POST: https://localhost:44342/api/Clientes
+GET: https://localhost:44342/api/Clientes/1
+PUT: https://localhost:44342/api/Clientes/1
+DELETE : https://localhost:44342/api/Clientes/1
